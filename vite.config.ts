3a500691ee870528469d8e80edefa65b61ae356d
lateef123442 <umuyetuahmed@gmail.com>
2026-05-14 18:48:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    TanStackRouterVite(),
     react(),
-    tailwindcss(),
-    cloudflare(),
   ],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  build: { outDir: "dist" },
-});
+})
